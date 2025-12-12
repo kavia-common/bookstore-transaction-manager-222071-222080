c0ee0159e2c54d890fef445d6214ee3ecf839136
Kavia AI <production@kavia.ai>
2025-12-12 03:41:03 +0000

    Checkpoint 1 - cg0ec6539a

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/bookstore-transaction-manager-222071-222080/bookstore_frontend
+flutter analyze
+
---
+++ bookstore_frontend/android/app/src/main/kotlin/com/example/bookstore_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bookstore_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
